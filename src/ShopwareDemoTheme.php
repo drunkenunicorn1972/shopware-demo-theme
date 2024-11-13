779b642a700230594aeda452135510fce7d69385
Drunken Unicorn <1425653+drunkenunicorn1972@users.noreply.github.com>
2024-11-13 14:48:21 +0100

    Initial commit

---
+++ src/ShopwareDemoTheme.php
@@ -0,0 +1,10 @@
+<?php declare(strict_types=1);
+
+namespace ShopwareDemoTheme;
+
+use Shopware\Core\Framework\Plugin;
+use Shopware\Storefront\Framework\ThemeInterface;
+
+class ShopwareDemoTheme extends Plugin implements ThemeInterface
+{
+}
